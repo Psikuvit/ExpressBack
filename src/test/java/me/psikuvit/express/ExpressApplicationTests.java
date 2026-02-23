fdca91cf7be8f554780dd17f28ad48841b976f47
Psikuvit <thegengestar@gmail.com>
2026-02-24 00:09:37 +0100

    Add rate limiting filter and service for API request management

---
+++ src/test/java/me/psikuvit/express/ExpressApplicationTests.java
@@ -0,0 +1,13 @@
+package me.psikuvit.express;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpressApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
