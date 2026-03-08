a1ed578b116d3c27946fdfae0374342f8b892a78
Psikuvit <thegengestar@gmail.com>
2026-03-08 02:03:35 +0100

    Feature: Enable asynchronous processing in ExpressApplication

--- src/main/java/me/psikuvit/express/ExpressApplication.java
+++ src/main/java/me/psikuvit/express/ExpressApplication.java
@@ -2,8 +2,10 @@ package me.psikuvit.express;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class ExpressApplication {
 
     public static void main(String[] args) {
